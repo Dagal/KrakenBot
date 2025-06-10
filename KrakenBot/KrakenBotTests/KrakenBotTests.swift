382198f04c1b6f19de49467f4ed65652371da018
Dagal <dejarding@gmail.com>
2025-06-10 15:07:38 +0200

    Add initial project files and structure for KrakenBot application

---
+++ KrakenBot/KrakenBotTests/KrakenBotTests.swift
@@ -0,0 +1,17 @@
+//
+//  KrakenBotTests.swift
+//  KrakenBotTests
+//
+//  Created by azerty on 10/06/2025.
+//
+
+import Testing
+@testable import KrakenBot
+
+struct KrakenBotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
